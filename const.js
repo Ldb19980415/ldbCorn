560a7150e7473cbf5e72c30ee274ada560ca4e4c
liudabiao <liudabiao@micstack.com>
2024-08-21 17:52:45 +0800

    配置防止tsc命令把ts文件转成js文件

--- const.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendInfoUrl = exports.ldbWeChatKey = exports.searchUrl = void 0;
-exports.searchUrl = "https://www.qxn.gov.cn/zwgk/zfjg/zrlzyshbzj/bmxxgkml_5135118/sydwkl_5135125/index.html";
-exports.ldbWeChatKey = "SCT212242TQ2WvUvD25g9Y3BPAWkJpGl4d";
-exports.sendInfoUrl = `https://sctapi.ftqq.com/${exports.ldbWeChatKey}.send`;
